d36b3aea73dac63a04cf403f70b9b95314840c13
Jeremy Shore <xboxmusicn3rd@gmail.com>
2013-01-31 23:51:57 -0600

    should skip useless IDs faster now.

--- GathererDBMaker/Program.cs
+++ GathererDBMaker/Program.cs
@@ -95,100 +95,100 @@ namespace GathererDBMaker
                 using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
                 {
                     string source = reader.ReadToEnd();
-                    getInfo(source, i); 
+                    if (source.Contains("id=\"ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage\"") == true)
+                    {
+                        getInfo(source, i);
+                    }
                 }
             }
         }
         
         static void getInfo(string source, int i)
         {
-            if (source.Contains("id=\"ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage\""))
+            int multiverseid = i;
+            string name = null;
+            string convmanacost = "";
+            string type = null;
+            string cardtext = "";
+            string power = "";
+            string toughness = "";
+            string expansion = null;
+            string rarity = null;
+            string imgurl = "";
+
+            //Gets card name from web page source and removes the linebreaks and tabs
+            int titlestart = source.IndexOf("<title>") + 7;
+            int titleend = source.IndexOf("</title>") - 1;
+            name = source.Substring(titlestart, (titleend - titlestart));
+            name = name.Substring(0, name.IndexOf(" - Gatherer - Magic: The Gathering"));
+            name = name.Replace("\n", string.Empty);
+            name = name.Replace("\r", string.Empty);
+            name = name.Replace("\t", string.Empty);
+            name = name.Trim();
+
+            if (source.Contains("Converted Mana Cost:</div>") == true)
             {
-                int multiverseid = i;
-                string name = null;
-                string convmanacost = "";
-                string type = null;
-                string cardtext = "";
-                string power = "";
-                string toughness = "";
-                string expansion = null;
-                string rarity = null;
-                string imgurl = "";
-
-                //Gets card name from web page source and removes the linebreaks and tabs
-                int titlestart = source.IndexOf("<title>") + 7;
-                int titleend = source.IndexOf("</title>") - 1;
-                name = source.Substring(titlestart, (titleend - titlestart));
-                name = name.Substring(0, name.IndexOf(" - Gatherer - Magic: The Gathering"));
-                name = name.Replace("\n", string.Empty);
-                name = name.Replace("\r", string.Empty);
-                name = name.Replace("\t", string.Empty);
-                name = name.Trim();
-
-                if (source.Contains("Converted Mana Cost:</div>") == true)
-                {
-                    int manacoststart = source.IndexOf("Converted Mana Cost:</div>");
-                    int manacostend = source.IndexOf("<br />", manacoststart);
-                    convmanacost = source.Substring(manacoststart, (manacostend - manacoststart));
-                    convmanacost = convmanacost.Substring((convmanacost.IndexOf("\">") + 2), (convmanacost.Length - (convmanacost.IndexOf("\">") + 2)));
-                    convmanacost = convmanacost.Replace("\n", string.Empty);
-                    convmanacost = convmanacost.Replace("\r", string.Empty);
-                    convmanacost = convmanacost.Replace("\t", string.Empty);
-                    convmanacost = convmanacost.Trim();
-                }
-
-                //Gets the card's type
-                int typestart = source.IndexOf("Types:</div>");
-                int typeend = source.IndexOf("</div>", (typestart +15));
-                type = source.Substring(typestart, (typeend - typestart));
-                type = type.Substring((type.IndexOf(">", 15) + 1), (type.Length - (type.IndexOf(">", 15) + 1)));
-                type = type.Replace("\n", string.Empty);
-                type = type.Replace("\r", string.Empty);
-                type = type.Replace("\t", string.Empty);
-                type = type.Trim();
-
-                //Gets the card's image
-                imgurl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + i + "&type=card";
-                /*int imgstart = source.IndexOf("<td class=\"leftCol\" align=\"center\">");
-                int imgend = source.IndexOf("/>", imgstart);
-                imgurl = source.Substring(imgstart, (imgend - imgstart));*/
-
-                if (source.Contains("P/T:</div>") == true)
-                {
-                    int PTstart = source.IndexOf("P/T:</div>");
-                    int PTend = source.IndexOf("</div>", PTstart + 10);
-                    string PT = source.Substring(PTstart, (PTend - PTstart));
-                    PT = PT.Substring((PT.IndexOf("\">") + 2), (PT.Length - (PT.IndexOf("\">")+2)));
-                    PT = PT.Replace("\n", string.Empty);
-                    PT = PT.Replace("\r", string.Empty);
-                    PT = PT.Replace("\t", string.Empty);
-                    PT = PT.Replace(" ", string.Empty);
-                    string[] PTsplit = PT.Split('/');
-                    power = PTsplit[0];
-                    toughness = PTsplit[1];
-                }
-
-                //Gets the Cards Rarity
-                int raritystart = source.IndexOf("Rarity:</div>");
-                int rarityend = source.IndexOf("</span>", raritystart);
-                rarity = source.Substring(raritystart, (rarityend - raritystart));
-                rarity = rarity.Substring(rarity.IndexOf("'>") + 2, (rarity.Length - rarity.IndexOf("'>")) - 2);
-                rarity = rarity.Trim();
-
-                //If the title contains parenthesis it cuts it out and puts what was inside into expansion
-                if (name.Contains("(") == true)
-                {
-                    expansion = name.Substring(name.IndexOf("(")+1, (name.IndexOf(")") - name.IndexOf("("))-1);
-                    name = name.Substring(0, name.IndexOf("("));
-                    name = name.Trim();
-                }
-
-                saveCard(multiverseid, name, convmanacost, type, cardtext, power, toughness, expansion, rarity, imgurl);
-                if (incLegality == true)
-                    getLegalitySource(multiverseid);
-
-                Console.WriteLine(name + "was added to the database.");
+                int manacoststart = source.IndexOf("Converted Mana Cost:</div>");
+                int manacostend = source.IndexOf("<br />", manacoststart);
+                convmanacost = source.Substring(manacoststart, (manacostend - manacoststart));
+                convmanacost = convmanacost.Substring((convmanacost.IndexOf("\">") + 2), (convmanacost.Length - (convmanacost.IndexOf("\">") + 2)));
+                convmanacost = convmanacost.Replace("\n", string.Empty);
+                convmanacost = convmanacost.Replace("\r", string.Empty);
+                convmanacost = convmanacost.Replace("\t", string.Empty);
+                convmanacost = convmanacost.Trim();
             }
+
+            //Gets the card's type
+            int typestart = source.IndexOf("Types:</div>");
+            int typeend = source.IndexOf("</div>", (typestart +15));
+            type = source.Substring(typestart, (typeend - typestart));
+            type = type.Substring((type.IndexOf(">", 15) + 1), (type.Length - (type.IndexOf(">", 15) + 1)));
+            type = type.Replace("\n", string.Empty);
+            type = type.Replace("\r", string.Empty);
+            type = type.Replace("\t", string.Empty);
+            type = type.Trim();
+
+            //Gets the card's image
+            imgurl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + i + "&type=card";
+            /*int imgstart = source.IndexOf("<td class=\"leftCol\" align=\"center\">");
+            int imgend = source.IndexOf("/>", imgstart);
+            imgurl = source.Substring(imgstart, (imgend - imgstart));*/
+
+            if (source.Contains("P/T:</div>") == true)
+            {
+                int PTstart = source.IndexOf("P/T:</div>");
+                int PTend = source.IndexOf("</div>", PTstart + 10);
+                string PT = source.Substring(PTstart, (PTend - PTstart));
+                PT = PT.Substring((PT.IndexOf("\">") + 2), (PT.Length - (PT.IndexOf("\">")+2)));
+                PT = PT.Replace("\n", string.Empty);
+                PT = PT.Replace("\r", string.Empty);
+                PT = PT.Replace("\t", string.Empty);
+                PT = PT.Replace(" ", string.Empty);
+                string[] PTsplit = PT.Split('/');
+                power = PTsplit[0];
+                toughness = PTsplit[1];
+            }
+
+            //Gets the Cards Rarity
+            int raritystart = source.IndexOf("Rarity:</div>");
+            int rarityend = source.IndexOf("</span>", raritystart);
+            rarity = source.Substring(raritystart, (rarityend - raritystart));
+            rarity = rarity.Substring(rarity.IndexOf("'>") + 2, (rarity.Length - rarity.IndexOf("'>")) - 2);
+            rarity = rarity.Trim();
+
+            //If the title contains parenthesis it cuts it out and puts what was inside into expansion
+            if (name.Contains("(") == true)
+            {
+                expansion = name.Substring(name.IndexOf("(")+1, (name.IndexOf(")") - name.IndexOf("("))-1);
+                name = name.Substring(0, name.IndexOf("("));
+                name = name.Trim();
+            }
+
+            saveCard(multiverseid, name, convmanacost, type, cardtext, power, toughness, expansion, rarity, imgurl);
+            if (incLegality == true)
+                getLegalitySource(multiverseid);
+
+            Console.WriteLine(name + " was added to the database.");
         }
 
         static void getLegalitySource(int multiversid)
